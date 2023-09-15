caa4da86b1bbe1a5405219bf27922c5dea7b1611
Lucas Rosa <lucas.wittdarosa@hotmail.com>
2023-09-15 17:13:01 -0300

    feat: configuring database with docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,25 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { UserTypeModule } from './user-type/user-type.module';
-
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [AuthModule, UsersModule, UserTypeModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+    }),
+    AuthModule,
+    UsersModule,
+    UserTypeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
